7bad50dbe7b326b16c5e21d9b9366f5d1d00ef78
Froylán Sánchez <froysm@gmail.com>
2013-08-12 20:55:28 -0500

    Add more customizations to the init.el file.

--- init.el
+++ init.el
@@ -1,6 +1,9 @@
 (setq fs-emacs-init-file load-file-name)
 (setq fs-emacs-config-dir
       (file-name-directory fs-emacs-init-file))
+(setq user-emacs-directory fs-emacs-config-dir)
+(setq backup-directory-alist
+      (list (cons "." (expand-file-name "backup" user-emacs-directory))))
 
 ;; Set up 'custom' system
 (setq custom-file (expand-file-name "emacs-customizations.el" fs-emacs-config-dir))
