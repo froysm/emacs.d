2c5c137d8fbf5fb1b23c6a16af94b8e5dcc18757
Froylán Sánchez <froysm@gmail.com>
2013-08-13 13:41:34 -0500

    Adds Marmalade and Magit.

--- init.el
+++ init.el
@@ -4,6 +4,11 @@
 (setq user-emacs-directory fs-emacs-config-dir)
 (setq backup-directory-alist
       (list (cons "." (expand-file-name "backup" user-emacs-directory))))
+(require 'package)
+(add-to-list 'package-archives
+	     '("marmalade" .
+	       "http://marmalade-repo.org/packages/"))
+(package-initialize)
 
 ;; Set up 'custom' system
 (setq custom-file (expand-file-name "emacs-customizations.el" fs-emacs-config-dir))
