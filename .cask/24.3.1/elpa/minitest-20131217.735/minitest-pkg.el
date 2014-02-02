e4a66f36d04ec0910d4ef939e1c03596b358127e
Froylán Sánchez <froysm@gmail.com>
2014-02-02 09:33:21 -0600

    Add Cask and a lot of dependencies.

---
+++ .cask/24.3.1/elpa/minitest-20131217.735/minitest-pkg.el
@@ -0,0 +1,1 @@
+(define-package "minitest" "20131217.735" "An Emacs mode for ruby minitest files" (quote ((dash "1.0.0"))))
--- init.el
+++ init.el
@@ -10,6 +10,156 @@
 	       "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
-;; Set up 'custom' system
-(setq custom-file (expand-file-name "emacs-customizations.el" fs-emacs-config-dir))
-(load custom-file)
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+
+;; Monokai theme
+(load-theme 'monokai t)
+
+;; Enhanced Ruby Mode
+(autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
+(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
+(add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
+
+;; Programming hooks
+(add-hook 'enh-ruby-mode-hook 'electric-pair-mode)
+(add-hook 'enh-ruby-mode-hook 'robe-mode)
+
+;; Emacs for Linux (strip tease)
+
+;; Prevent the cursor from blinking
+(blink-cursor-mode 0)
+;; Don't use messages that you don't read
+(setq initial-scratch-message "")
+(setq inhibit-startup-message t)
+;; Don't let Emacs hurt your ears
+(setq visible-bell t)
+
+;; You need to set `inhibit-startup-echo-area-message' from the
+;; customization interface:
+;; M-x customize-variable RET inhibit-startup-echo-area-message RET
+;; then enter your username
+(setq inhibit-startup-echo-area-message "froysm")
+
+;; This is bound to f11 in Emacs 24.4
+;; (toggle-frame-fullscreen) 
+;; Who use the bar to scroll?
+(scroll-bar-mode 0)
+
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+
+;; You can also set the initial frame parameters
+;; (setq initial-frame-alist
+;;       '((menu-bar-lines . 0)
+;;         (tool-bar-lines . 0)))
+
+;; See http://bzg.fr/emacs-hide-mode-line.html
+(defvar-local hidden-mode-line-mode nil)
+(defvar-local hide-mode-line nil)
+
+(define-minor-mode hidden-mode-line-mode
+  "Minor mode to hide the mode-line in the current buffer."
+  :init-value nil
+  :global nil
+  :variable hidden-mode-line-mode
+  :group 'editing-basics
+  (if hidden-mode-line-mode
+      (setq hide-mode-line mode-line-format
+            mode-line-format nil)
+    (setq mode-line-format hide-mode-line
+          hide-mode-line nil))
+  (force-mode-line-update)
+  ;; Apparently force-mode-line-update is not always enough to
+  ;; redisplay the mode-line
+  (redraw-display)
+  (when (and (called-interactively-p 'interactive)
+             hidden-mode-line-mode)
+    (run-with-idle-timer
+     0 nil 'message
+     (concat "Hidden Mode Line Mode enabled.  "
+             "Use M-x hidden-mode-line-mode to make the mode-line appear."))))
+
+;; Activate hidden-mode-line-mode
+(hidden-mode-line-mode 1)
+
+;; If you want to hide the mode-line in all new buffers
+;; (add-hook 'after-change-major-mode-hook 'hidden-mode-line-mode)
+
+;; Activate hidden-mode-line-mode
+(hidden-mode-line-mode 1)
+
+;; If you want to hide the mode-line in all new buffers
+;; (add-hook 'after-change-major-mode-hook 'hidden-mode-line-mode)
+
+;; Alternatively, you can paint your mode-line in White but then
+;; you'll have to manually paint it in black again
+;; (custom-set-faces
+;;  '(mode-line-highlight ((t nil)))
+;;  '(mode-line ((t (:foreground "white" :background "white"))))
+;;  '(mode-line-inactive ((t (:background "white" :foreground "white")))))
+
+;; A small minor mode to use a big fringe
+(defvar bzg-big-fringe-mode nil)
+(define-minor-mode bzg-big-fringe-mode
+  "Minor mode to hide the mode-line in the current buffer."
+  :init-value nil
+  :global t
+  :variable bzg-big-fringe-mode
+  :group 'editing-basics
+  (if (not bzg-big-fringe-mode)
+      (set-fringe-style nil)
+    (set-fringe-mode
+     (/ (- (frame-pixel-width)
+           (* 100 (frame-char-width)))
+        2))))
+
+;; Now activate this global minor mode
+(bzg-big-fringe-mode 1)
+
+;; To activate the fringe by default and deactivate it when windows
+;; are split vertically, uncomment this:
+;; (add-hook 'window-configuration-change-hook
+;;           (lambda ()
+;;             (if (delq nil
+;;                       (let ((fw (frame-width)))
+;;                         (mapcar (lambda(w) (< (window-width w) fw))
+;;                                 (window-list))))
+;;                 (bzg-big-fringe-mode 0)
+;;               (bzg-big-fringe-mode 1))))
+
+;; Use a minimal cursor
+;; (setq cursor-type 'hbar)
+
+;; Get rid of the indicators in the fringe
+;; (mapcar (lambda(fb) (set-fringe-bitmap-face fb 'org-hide))
+;;        fringe-bitmaps)
+
+;; Set the color of the fringe
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background "black" :foreground "grey"))))
+ '(fringe ((t (:background "black")))))
+
+
+
+;; Command to toggle the display of the mode-line as a header
+(defvar-local header-line-format nil)
+(defun mode-line-in-header ()
+  (interactive)
+  (if (not header-line-format)
+      (setq header-line-format mode-line-format
+            mode-line-format nil)
+    (setq mode-line-format header-line-format
+          header-line-format nil))
+  (set-window-buffer nil (current-buffer)))
+(global-set-key (kbd "C-s-SPC") 'mode-line-in-header)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("9c26d896b2668f212f39f5b0206c5e3f0ac301611ced8a6f74afe4ee9c7e6311" default))))
